d5fa578a65f7d336f1a6177f0fc9d2d1f1ec35e6
mail.suryateja@gmail.com <suryasonti@Suryas-MacBook-Pro.local>
2018-02-01 15:18:21 -0800

    Changed file description

--- PiFormulas/src/PiFormulas.cpp
+++ PiFormulas/src/PiFormulas.cpp
@@ -3,7 +3,7 @@
 // Author      : SuryaTSonti
 // Version     :
 // Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
+// Description : Pi Formulas from Ramanujam and Euler in C++, Ansi-style
 //============================================================================
 
 //includes
