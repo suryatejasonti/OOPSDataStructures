79200b43959e336e01bf3961b10e9f15c374d9be
mail.suryateja@gmail.com <suryasonti@Suryas-MacBook-Pro.local>
2018-02-19 05:08:08 -0800

    added practice paper

---
+++ BigPi/src/BigPi.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : BigPi.cpp
+// Author      : SuryaTSonti
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Calculating Pi using Borwein algorithm in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
